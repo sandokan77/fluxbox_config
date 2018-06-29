f800a075ab8e32c3bdecf2edba8ad2f05d3c8d6b
mk <mk@gmail.com>
2018-06-30 06:58:21 +1200

    .

--- do_backup_fluxbox.sh
+++ do_backup_fluxbox.sh
@@ -2,3 +2,4 @@
 cp ~/.fluxbox/startup ~/.fluxbox/startup.orig
 cp ~/.fluxbox/menu ~/.fluxbox/menu.orig 
 cp ~/.fluxbox/keys ~/.fluxbox/keys.orig 
+cp ~/.fluxbox/init ~/.fluxbox/init.orig
--- do_update_fluxbox.sh
+++ do_update_fluxbox.sh
@@ -2,3 +2,4 @@
 cp startup ~/.fluxbox/startup
 cp menu ~/.fluxbox/menu
 cp keys ~/.fluxbox/keys
+cp init ~/.fluxbox/init
